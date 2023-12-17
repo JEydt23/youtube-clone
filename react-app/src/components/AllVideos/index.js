7300c36a0d042ee1a9874f93cc9d310d8cee7f90
jeydt23 <jeydt23@gmail.com>
2023-12-16 22:12:51 -0500

    allvids showing!

--- react-app/src/components/AllVideos/index.js
+++ react-app/src/components/AllVideos/index.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllVideos } from "../../store/video";
-// import AllVideos from './AllVideos';
 import ReactPlayer from 'react-player'
+import './AllVideos.css'
 
 const ShowAllVideos = () => {
     const videoState = useSelector(state => state.video.viewAllVideos);
@@ -14,34 +14,20 @@ const ShowAllVideos = () => {
 
     }, [dispatch, videoState.id])
 
-    // if (!videoState === null) return <div>Loading...</div>;
 
-    // if (Object.keys(videoState).length === 0) {
-    //     // Handle the case where there are no videos
-    //     return <div>No videos available.</div>;
-    // }
+
+
     return (
-        <div>
-        {/* className="videos-list" style={{ border: 'red', display: 'flex' } */}
-            {Object.values(videoState).map(video => {
-                <div>hi</div>
-                // { console.log(video.video_url, 'video') }
+        <div className="videos-div" style={{display: 'flex', flexWrap: 'wrap'}} >
+           {Object.values(videoState).map(video => (
+                <div className="each-vid">
+                    <ReactPlayer url={video.video_url} controls='true' wrapper="div" style={{ margin: '5px' }} />
 
-                // <div>
-                //     {/* {video.video_url} */}
-                // <div>
-                //     {/* {video.video_url} */}
-                //     <div className="allVideos" style={{ border: 'red', display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
-                //         <ReactPlayer video={video.video_url} wrapper='div' width="320px" height="240px" controls={true} style={{ margin: '5px', border: 'blue' }} />
-                //         <video src={video.video_url} />
-                //     </div>
+                </div>
 
-                // </div>
+            ))}
 
-                // </div>
-            })}
 
-            hi
         </div>
 
     )
@@ -51,3 +37,23 @@ const ShowAllVideos = () => {
 }
 
 export default ShowAllVideos;
+
+// if (!videoState === null) return <div>Loading...</div>;
+// { console.log(video.video_url, 'video') }
+
+                // <div>
+                //     {/* {video.video_url} */}
+                // <div>
+                //     {/* {video.video_url} */}
+                //     <div className="allVideos" style={{ border: 'red', display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+                //         <video src={video.video_url} />
+                //     </div>
+
+                // </div>
+
+                // </div>
+
+                // if (Object.keys(videoState).length === 0) {
+    //     // Handle the case where there are no videos
+    //     return <div>No videos available.</div>;
+    // }
